2a4befb15784361e2f324636eef9c19283b09c34
taylor.martinez <taylor.swot.2018@gmail.com>
2023-10-24 21:44:16 -0500

    updated project README

--- Develop/server.js
+++ Develop/server.js
@@ -1,27 +1,27 @@
 // import express package
 const express = require('express');
 const path = require('path');
-const getNotes = require('.Develop/public/index.js');
+const getNotes = require('./Develop/public/assets/js/index.js');
 const PORT = 3001;
 const app = express();
 
 
 // static middleware pointing to the 'public' folder
-app.use(express.static('develop/public'));
-
-// create http methods for the various actions of the note taker app and then add the route handlers that correlate to the task what is being requested
-// use sendFile to send the public file and path.join to link the server and the files you want to display in the browser to the client and use __dirname to get the absolute path so that when you deploy you don't have your stuff on the internet
+app.use(express.static('Develop/public'));
+app.use(express.json());
+app.use(express.urlencoded())
 
 // created GET route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 );
 
 // created GET route for notes page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
 );
 
+// create GET route to getNotes
 app.get('/api/notes', (req, res) => res.json(getNotes));
 
 // check to see if the server is active
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # note-taker-repo
+
+## Description
+A note taker application to allow users to stay organized
+
+## Assets
+
+## Installation
+Install the express.js package by running the command 'npm install' in the command line
+
+## Technologies
+* node.js
+* express
+
+## Usage
+To use this application navigate to the browser. In the command line install the express.js package in the command line. Users will be directed to the landing page with a 'Get Started' button. When a user clicks this button they will be redirected to the notes page where they can view existing notes, create a new note, or delete old notes. Notes will be stored in the server until the user deletes them.
+
+## Credits
+Received starter code from https://github.com/coding-boot-camp/miniature-eureka and utilized the Xpert Learning Assistant.
+
+## License
+This project is covered under the MIT license
+
+## Special Note
+This application is still being updated and developed
